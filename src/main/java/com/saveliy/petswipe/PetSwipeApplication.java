985406920703e32649442a52988f9f113c5df9cc
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2025-03-09 23:12:42 +0300

    First commit

---
+++ src/main/java/com/saveliy/petswipe/PetSwipeApplication.java
@@ -0,0 +1,13 @@
+package com.saveliy.petswipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSwipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetSwipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saveliy/petswipe/PetSwipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saveliy.petswipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSwipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
